0ea23e386e4594ed0ba99073817bccf4c3a0e152
Fabien Benetou <fabien-services@benetou.fr>
2015-04-17 09:39:25 +0200

    linted

--- MakeMe3DObject.py
+++ MakeMe3DObject.py
@@ -1,28 +1,35 @@
+''' Creating a new 3D printable object'''
+
 # find libraries of objects, blend them together in 3D, look for typical printable issue (e.g. too thin)
 #   add typical simple solutions to each problem
-import sys
 import argparse
 
 # import numpy as np
 # import matplotlib.pyplot as plt
 # from stl_tools import numpy2stl
 
-class library_of_meshes(object):
+class LibraryOfMeshes(object):
+    ''' Managing a library of meshes '''
 
     def __init__(self):
+        ''' Create a new library of meshes, eventually load a saved one '''
         pass
 
     # load user theme or parameters as keywords
     # renamed from find_meshes_from_keywords()
-    def extend(self, list_of_keywords):
+    def Extend(self, list_of_keywords):
+        ''' Find a mesh based on keywords then add it '''
         pass
 
-    def get_random(self):
+    def GetRandom(self):
+        ''' Return a random mesh from the current library '''
         pass
 
-class printable_new_mesh(object):
+class PrintableNewMesh(object):
+    ''' Create a new mesh that can be 3D printed '''
 
     def __init__(self, mesh):
+        ''' Create a new mesh based on an existing one '''
         pass
 
     # "conify" function would provide a good basis for a lamp from a 3D object
@@ -30,22 +37,36 @@ class printable_new_mesh(object):
     # "tabletify" function would provide a good basis for a tablet or phone holder from a 3D object
     #   simply by substracting a rectangle from the approximate size (usually given in inches) to the top of it
     # function to transform a mesh to provide affordances (e.g. wearable on wrist, holding a tablet, etc)
-    def provide_affordance(self, affordance):
+    def ProvideAffordance(self, affordance):
+        ''' Insure that a mesh has a shape that provide a useful function to its user '''
+        if affordance == "hold_a_lightbulb":
+            pass
         pass
 
-    def is_not_3Dprintable(self):
-        pass 
-
-    def fix_printability_issues(self):
+    def IsNot3DPrintable(self):
+        ''' Find problems related to 3D printing '''
         pass
 
-    def blend_with_other_meshes(self, list_of_meshes):
+    def FixPrintabilityIssues(self):
+        ''' Fix problems related to 3D printing '''
         pass
 
-    def render_preview(self):
+    # can be blended along
+        # center of each object (merging)
+        # border of each object (chaining)
+        # a solid axis (holding stick)
+        # helix
+        # other supporting structures
+    def BlendWithOtherMeshes(self, list_of_meshes):
+        ''' Blend multiple meshes with the current one '''
         pass
 
-    def export_STL(self, filename):
+    def RenderPreview(self):
+        ''' Render as images a mesh '''
+        pass
+
+    def ExportSTL(self, filename):
+        ''' Export the current mesh to the STL format '''
         pass
 
 # generate a bunch of models with 3D preview
@@ -53,7 +74,7 @@ class printable_new_mesh(object):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Module to create a new 3D-printable object.")
-    parser.add_argument("-v", "--verbose", action="store_true", default = False, 
+    parser.add_argument("-v", "--verbose", action="store_true", default=False,
                         help="Verbose, display plenty of debugging and testing information")
     parser.add_argument("-t", "--theme", default="cube",
                         help="Theme to be used as inspiration")
@@ -67,12 +88,12 @@ if __name__ == "__main__":
     if args.verbose:
         print("Verbose mode")
 
-    MyLibrary = library_of_meshes()
-    MyLibrary.extend(args.theme)
-    MyMesh = printable_new_mesh(MyLibrary.get_random)
-    MyMesh.blend_with_other_meshes(MyLibrary.get_random)
-    MyMesh.provide_affordance(args.affordance)
-    while MyMesh.is_not_3Dprintable():
-        MyMesh.fix_printability_issues()
-    MyMesh.render_preview()
-    MyMesh.export_STL(args.filename)
+    MyLibrary = LibraryOfMeshes()
+    MyLibrary.Extend(args.theme)
+    MyMesh = PrintableNewMesh(MyLibrary.GetRandom)
+    MyMesh.BlendWithOtherMeshes(MyLibrary.GetRandom)
+    MyMesh.ProvideAffordance(args.affordance)
+    while MyMesh.IsNot3DPrintable():
+        MyMesh.FixPrintabilityIssues()
+    MyMesh.RenderPreview()
+    MyMesh.ExportSTL(args.filename)
